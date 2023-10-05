a21bd069808586696e3e87834d5057dee775b0cf
Uti-Favour <utifavour15@gmail.com>
2023-10-05 15:53:10 +0100

    Changes made today

--- DOM.js
+++ DOM.js
@@ -1,7 +1,1 @@
-let btn = document.getElementById("btn");
-function displayMsg(){
-    console.log("Button Clicked");
-}
-btn.addEventListener('click' , displayMsg);
 
-btn.removeEventListener('click' , displayMsg);
