7908828591c8c8276619e292f10da87c4eb09bc8
Uti-Favour <utifavour15@gmail.com>
2023-10-06 23:08:43 +0100

    Events

---
+++ Asynchronous.js
@@ -0,0 +1,8 @@
+const element = document.getElementById('myElement'); // Replace 'myElement' with the ID of your HTML element
+element.addEventListener('click', function(event) {
+  // Your code to handle the click event goes here
+});
+
+
+//Used to remove event listeners
+element.removeEventListener('click', handleClick);
