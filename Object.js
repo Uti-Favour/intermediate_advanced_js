9548ad11b5f89715d24f8e1062c24ebf67cc2c29
Uti-Favour <utifavour15@gmail.com>
2023-10-11 18:36:27 +0100

    New constructor

--- Object.js
+++ Object.js
@@ -118,20 +118,29 @@ console.log(calculator.sum());
 console.log(calculator.mul());
 
 //Chain Object Project
-let ladder = {
-    step: 0,
-    up() {
-      this.step++;
-      return this
-    },
-    down() {
-      this.step--;
-      return this
-    },
-    showStep: function() { // shows the current step
-      alert( this.step );
-      return this
-    }
-  };
+//let ladder = {
+  //  step: 0,
+//     up() {
+//       this.step++;
+//       return this
+//     },
+//     down() {
+//       this.step--;
+//       return this
+//     },
+//     showStep: function() { // shows the current step
+//       alert( this.step );
+//       return this
+//     }
+//   };
 
-  ladder.up().up().down().showStep().down().showStep();
+  //ladder.up().up().down().showStep().down().showStep();
+
+
+
+
+
+
+
+  //Constructor, operator "new"
+  
