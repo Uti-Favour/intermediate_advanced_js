7dd4cefc1e384f04664d414a25114ac92ce2eca8
Uti-Favour <utifavour15@gmail.com>
2023-12-04 23:03:01 +0100

    DOM manipulation

---
+++ DOM.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded' , function(){
+    const first = document.querySelector('ul');
+const second = document.querySelectorAll('.child');
+document.getElementById('parent');
+console.log(first);
+console.log(second);
+
+document.body.style.backgroundColor = 'blue';
+setTimeout(() => document.body.style.backgroundColor = '' , 1000);
+
+
+});
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+
+let element = document.querySelector('.element');
+
+element.hidden = true;
+
+
+
+
+
+
+
+
