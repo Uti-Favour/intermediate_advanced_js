04024e168280fdb7dfe953698802ebd28413c05b
Uti-Favour <utifavour15@gmail.com>
2023-12-05 12:44:22 +0100

    Modifying html document with DOM

--- DOM.js
+++ DOM.js
@@ -9,8 +9,6 @@ document.body.style.backgroundColor = 'blue';
 setTimeout(() => document.body.style.backgroundColor = '' , 1000);
 
 
-});
-
 console.log(window.innerHeight);
 console.log(window.innerWidth);
 
@@ -20,9 +18,27 @@ let element = document.querySelector('.element');
 element.hidden = true;
 
 
+//Modifying HTML element with JavaScript
+let div = document.createElement('div');
+
+//adding classname
+div.className = "message";
+
+//inner content
+div.innerHTML = "<strong>Hi there!</strong> you got a new message";
 
 
+document.body.append(div);
+
+document.body.insertAdjacentHTML('afterbegin' , `<div class = 'message'><strong>Hi there!</strong> You have a new message!</div>`);
+
+setTimeout(() => {div.remove() }, 5000);
+
+//Adjcent
+const adj = document.querySelector('#adjacent');
+adj.insertAdjacentHTML("beforebegin" , "<p>HappyYou</p>");
+adj.insertAdjacentHTML("afterbegin", "<p>endHappy</p>")
 
 
-
+});
 
